c4b8c3cb25f522b9fbc6016ebd9c1e332fea95fc
Wojciech Marek <marek.wojciech@outlook.com>
2023-08-03 18:37:21 +0200

    feat: add footer section

---
+++ src/views/Footer/Footer.styled.tsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled";
+
+export const FooterContainer = styled.div`
+  background-color: #161721;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 3em;
+`;
+
+export const FooterContent = styled.div`
+  max-width: 65em;
+  width: 90%;
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  display: flex;
+  gap: 0.5em;
+`;
+
+export const FooterBoldText = styled.p`
+  color: #fff;
+  font-weight: 900;
+`;
+
+export const FooterText = styled.p`
+  color: #fff;
+`;
+
+
+
