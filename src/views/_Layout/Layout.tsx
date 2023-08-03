a494181d4a6f3d1a8b3aff9b2494827892eb75fb
Wojciech Marek <marek.wojciech@outlook.com>
2023-08-03 18:37:21 +0200

    chore: move navbar component into more correct place

--- src/views/_Layout/Layout.tsx
+++ src/views/_Layout/Layout.tsx
@@ -1,11 +1,11 @@
 import { Landing } from "../Landing";
 import { Experience } from "../Experience";
-import { Navbar } from "../../components/organisms/Navbar/Navbar";
 import { About } from "../About";
 import { Projects } from "../Projects";
 import { Footer } from "../Footer";
 import { Contact } from "../Contact";
 import { Medium } from "../Medium";
+import { Navbar } from "../Navbar";
 
 export const Layout = () => {
   return (
