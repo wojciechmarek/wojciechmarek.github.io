5b71e2b3a76e7c518373e186f1bb7675e351326f
Wojciech Marek <marek.wojciech@outlook.com>
2023-08-03 18:37:21 +0200

    feat: add experience section

---
+++ src/views/Experience/Experience.styled.tsx
@@ -0,0 +1,167 @@
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const ExperienceContainer = styled.div`
+  background-color: #0b0c13;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ExperienceTitle = styled.h1`
+  font-size: 2rem;
+  font-weight: 900;
+  color: #fff;
+  margin: 1em 0;
+`;
+
+export const ExperienceContent = styled.div`
+  max-width: 65em;
+  width: 90%;
+  flex-grow: 1;
+  margin-bottom: 5em;
+  color: #fff;
+`;
+
+export const ExperiencePanel = styled.div`
+  display: flex;
+`;
+
+export const ExperiencePanelVerticalLine = styled.div`
+  /* width: 0.125em;
+  height: 20em;
+  background-color: white; */
+`;
+
+export const ExperiencePanelContent = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 2em;
+`;
+
+export const ExperiencePanelJobContainer = styled.div`
+  display: flex;
+  gap: 1em;
+`;
+
+export const ExperiencePanelJobDotContainer = styled.div`
+  margin-top: 0.2em;
+  height: 2em;
+  aspect-ratio: 1/1;
+  background-color: #0b0c13;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+export const ExperiencePanelJobDot = styled.div<{ isActive?: boolean }>`
+  position: absolute;
+  width: 1em;
+  height: 1em;
+  border-radius: 50%;
+  background-color: ${(props) => (props.isActive ? "#4cc933" : "#c9335d")};
+`;
+
+const pulsate = keyframes`
+    0% { -webkit-transform: scale(0.1, 0.1); opacity: 0.0;}
+    50% {opacity: 1.0;}
+    100% {-webkit-transform: scale(1.2, 1.2); opacity: 0.0;}
+`;
+
+export const ExperiencePanelJobDotRing = styled.div<{ isActive?: boolean }>`
+  display: ${(props) => (props.isActive ? "block" : "none")};
+  border: 3px solid #62bd19;
+  -webkit-border-radius: 30px;
+  height: 25px;
+  width: 25px;
+  position: absolute;
+  -webkit-animation: ${pulsate} 2s ease-out;
+  -webkit-animation-iteration-count: infinite;
+  opacity: 0;
+`;
+
+export const ExperiencePanelJobContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+`;
+
+export const ExperiencePanelJobData = styled.div``;
+
+export const ExperiencePanelJobCompanyName = styled.a`
+  font-size: 1.5rem;
+  font-weight: 800;
+  color: #fff;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+    color: #c9335d;
+  }
+`;
+
+export const ExperiencePanelJobPositionContainer = styled.div``;
+
+export const ExperiencePanelJobPositionData = styled.div`
+  display: flex;
+  gap: 1em;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    gap: 0;
+  }
+`;
+
+export const ExperiencePanelJobPositionDescription = styled.ul`
+  margin-top: 1.25em;
+  margin-left: 1em;
+  margin-bottom: 3em;
+`;
+
+export const ExperiencePanelJobPositionDescriptionItem = styled.li`
+  margin: 0.25em 0;
+`;
+
+export const ExperiencePanelJobPositionText = styled.div`
+  font-size: 1.25rem;
+  font-weight: 800;
+  color: #898989;
+
+  @media screen and (max-width: 768px) {
+    &.vertical_separator {
+      display: none;
+    }
+  }
+`;
+
+export const ExperiencePanelJobPositionTitle = styled.div``;
+
+export const ExperiencePanelJobDescription = styled.div``;
+
+export const ExperiencePanelJobTitleAndCompany = styled.p`
+  display: flex;
+  align-items: center;
+  gap: 2em;
+`;
+
+export const ExperiencePanelJobTitle = styled.div`
+  font-size: 1.25rem;
+  font-weight: 800;
+`;
+
+export const ExperiencePanelJobCompany = styled.div`
+  font-size: 1.75rem;
+  font-weight: 900;
+  color: white;
+`;
+
+export const ExperiencePanelJobDuration = styled.div`
+  font-size: 1rem;
+  font-weight: 600;
+  color: #9d9d9d;
+`;
+
+export const ExperiencePanelJobPosition = styled.div``;
---
+++ src/views/Experience/Experience.tsx
@@ -0,0 +1,150 @@
+import {
+  ExperienceContainer,
+  ExperienceContent,
+  ExperiencePanel,
+  ExperiencePanelContent,
+  ExperiencePanelJobCompanyName,
+  ExperiencePanelJobContainer,
+  ExperiencePanelJobContent,
+  ExperiencePanelJobData,
+  ExperiencePanelJobDot,
+  ExperiencePanelJobDotContainer,
+  ExperiencePanelJobDotRing,
+  ExperiencePanelJobPositionContainer,
+  ExperiencePanelJobPositionData,
+  ExperiencePanelJobPositionDescription,
+  ExperiencePanelJobPositionDescriptionItem,
+  ExperiencePanelJobPositionText,
+  ExperiencePanelVerticalLine,
+  ExperienceTitle,
+} from "./Experience.styled";
+
+const companies = [
+  {
+    name: "Xebia Poland (formerly as PGS Software)",
+    link: "https://xebia.com/",
+    isActive: true,
+    positions: [
+      {
+        name: "Frontend Web Developer",
+        duration: "May 2022 - now",
+        responsibilities: [
+          "Helping the frontend develops team to maintain a custom controls library for the client from the low-cost airline industry.",
+          "Developing a new features for existing internal system used to managed the company's projects and also logging a work time for employees.",
+          "Developing a greenfield project for an internal use to facilitate the process of generating notes during a technical interviews.",
+        ],
+      },
+    ],
+  },
+  {
+    name: "Fabrity",
+    link: "https://fabrity.com/",
+    isActive: false,
+    positions: [
+      {
+        name: "Regular Software Developer",
+        duration: "January 2021 - December 2021",
+        responsibilities: [
+          "Implementing a new features in the existing system. The system was a retail engine written in C# language and divided into several microservices. The communication between microservices was based on message broker (kafka) and orchestrated by Saga pattern. The project was for the company for medical services industry, on of the biggest in the central europe.",
+        ],
+      },
+      {
+        name: "Junior Software Developer",
+        duration: "January 2020 - January 2021",
+        responsibilities: [
+          "Rewriting an existing software written in the Universal Windows Platform into a new version (a greenfield project) written in Angular framework. The project was for one of the biggest companies from the pharmaceutical industry, headquartered in the United States of America.",
+          "Extending an existing project with new features. Software was written in ASP.NET MVC and was used to facilitate the generation crating and management of documents. The project was for the company from the building industry in Poland.",
+        ],
+      },
+    ],
+  },
+  {
+    name: "South Mountain YMCA Camps",
+    link: "https://smymca.org/",
+    isActive: false,
+    positions: [
+      {
+        name: "General Maintenance Staff",
+        duration: "June 2019 - August 2019",
+        responsibilities: [
+          "Keeping the camp clean and safe for the campers.",
+          "Maintaining the camp facilities.",
+          "Improving the English language skills.",
+        ],
+      },
+    ],
+  },
+  {
+    name: "{e-bi}",
+    link: "https://e-bi.pl/",
+    isActive: false,
+    positions: [
+      {
+        name: "Junior .Net Developer (working student)",
+        duration: "October 2018 - April 2019",
+        responsibilities: [
+          "Creating a Xamarin mobile application for existing CMS system.",
+          "Testing and maintaining a web application written in the Angular framework. The project gave the people to share their private parking lots.",
+        ],
+      },
+    ],
+  },
+];
+
+export const Experience = () => {
+  return (
+    <ExperienceContainer id="experience">
+      <ExperienceContent>
+        <ExperienceTitle>#my_experience</ExperienceTitle>
+        <ExperiencePanel>
+          <ExperiencePanelContent>
+            {companies.map((job) => (
+              <ExperiencePanelJobContainer>
+                <ExperiencePanelJobDotContainer>
+                  <ExperiencePanelJobDot isActive={job.isActive} />
+                  <ExperiencePanelJobDotRing isActive={job.isActive} />
+                </ExperiencePanelJobDotContainer>
+                <ExperiencePanelJobContent>
+                  <ExperiencePanelJobData>
+                    <ExperiencePanelJobCompanyName
+                      href={job.link}
+                      target="_blank"
+                    >
+                      {job.name}
+                    </ExperiencePanelJobCompanyName>
+                    {job.positions.map((position) => (
+                      <ExperiencePanelJobPositionContainer key={position.name}>
+                        <ExperiencePanelJobPositionData>
+                          <ExperiencePanelJobPositionText>
+                            {position.duration}
+                          </ExperiencePanelJobPositionText>
+                          <ExperiencePanelJobPositionText className="vertical_separator">
+                            |
+                          </ExperiencePanelJobPositionText>
+                          <ExperiencePanelJobPositionText>
+                            {position.name}
+                          </ExperiencePanelJobPositionText>
+                        </ExperiencePanelJobPositionData>
+                        <ExperiencePanelJobPositionDescription>
+                          {position?.responsibilities?.map(
+                            (responsibility: string) => (
+                              <ExperiencePanelJobPositionDescriptionItem
+                                key={responsibility}
+                              >
+                                {responsibility}
+                              </ExperiencePanelJobPositionDescriptionItem>
+                            )
+                          )}
+                        </ExperiencePanelJobPositionDescription>
+                      </ExperiencePanelJobPositionContainer>
+                    ))}
+                  </ExperiencePanelJobData>
+                </ExperiencePanelJobContent>
+              </ExperiencePanelJobContainer>
+            ))}
+          </ExperiencePanelContent>
+        </ExperiencePanel>
+      </ExperienceContent>
+    </ExperienceContainer>
+  );
+};
