a580234e61bd0bdfa2e733a5db90fdefc4439d4f
Wojciech Marek <wojciech.marek@xebia.com>
2023-08-03 18:37:21 +0200

    feat: update descriptions and icon

--- src/views/_Navbar/Navbar.tsx
+++ src/views/_Navbar/Navbar.tsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
           <NavbarLink href="#about">About</NavbarLink>
           <NavbarLink href="#experience">Experience</NavbarLink>
           <NavbarLink href="#projects">Projects</NavbarLink>
-          <NavbarLink href="#articles">Articles</NavbarLink>
+          {/* <NavbarLink href="#articles">Articles</NavbarLink> */}
           <NavbarLink href="#contact">Contact</NavbarLink>
           {/* <NavbarLink>☀</NavbarLink> */}
         </NavbarLinks>
